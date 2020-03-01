3a20a2d43595abe186c3e7b43f9b7b65fdf8e3ae
Drew Ulick <wren@Drews-MacBook-Pro.local>
2020-03-01 17:04:36 -0500

    added key testing

--- sidechannel.py
+++ sidechannel.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-
+from os import urandom
 import requests
 import pickle
+from aes import AES
+
+aes = AES()
 
 URL = "https://courses.csail.mit.edu/6.857/2020/6857_aes.php"
 payload = {"num": "1000"}
@@ -29,19 +32,12 @@ means = [0 for i in range(128)]
 not_means = [0 for i in range(128)]
 times_sum = 0
 n = 0
-
 for i in range(len(tuples)):
     s = tuples[i]
     r = s.split(",")
     if len(r) != 3:
-        print("Got short tuple, {}".format(r))
         continue
     pt, ct, time = r
-    if i == 1:
-        print(len(pt), len(ct))
-        print(pt)
-        print(ct)
-        print(time)
     times_sum += float(time)
     tuples[i] = (pt, ct, time)
     n += 1
@@ -49,45 +45,62 @@ for i in range(len(tuples)):
 times_mean = times_sum / n
 
 print("Times mean", times_mean)
+print(tuples[0])
+
+# string of space delimited ints to a list of ints
+def to_bytes(s):
+    s = s.replace("\n", "")
+    ct_bytes = filter(lambda x: x != "", s.split(" "))
+    ct_bytes = map(int, ct_bytes)
+    ct_bytes = list(ct_bytes)
+    return ct_bytes
+
 
 delayed = 0
 for t in tuples:
     if len(t) != 3 or type(t) == str:
-        print(t)
-        print("T HAS WRONG LENGTH YOu fUcKInG IDIIOT")
         continue
-    pt, ct, time = t
-    pt = pt.replace("\n","")
-    pt = pt.split(" ")
-    pt = list(filter(lambda x: x != '', pt))
+    pt, _, time = t
+    pt = to_bytes(pt)
     if float(time) > times_mean:
         delayed += 1
-
         for i in range(16):
-
-            b = int(pt[i], 16)
-            # print("pt is {} and has type {}".format(pt, type(pt)))
-            # print(type(pt[i]))
             for j in range(8):
-                # each bit of means is taking only one bit from the byte represented by pt
-                bitwise_res = ((b >> j) ^ 1) & 1
+                # each element of means is taking only one bit from the byte represented by pt
+                bitwise_res = ((pt[i] >> j) ^ 1) & 1
                 means[8 * i + j] += int(bitwise_res)
     else:
         for i in range(16):
-            b = int(pt[i], 16)
-            # print("pt is {} and has type {}".format(pt, type(pt)))
-            # print(type(pt[i]))
             for j in range(8):
-                # each bit of means is taking only one bit from the byte represented by pt
-                bitwise_res = ((b >> j) ^ 1) & 1
+                bitwise_res = ((pt[i] >> j) ^ 1) & 1
                 not_means[8 * i + j] += int(bitwise_res)
 
 
 print("There were {} delayed encryptions".format(delayed))
-print("n is {} means are {}".format(n,means))
+print("n is {} means are {}".format(n, means))
 # divide sums to get means
 for i in range(128):
     means[i] = means[i] / (not_means[i] + means[i])
+    if means[i] < 0.48:
+        means[i] = 0
+    else:
+        means[i] = 1
 
-print(means)
-print(''.join(map(str,means)))
+print("".join(map(str, means)))
+
+bytes = [0 for i in range(16)]
+for i in range(16):
+    bytes[i] = int("".join(map(str, means[8 * i : 8 * i + 8])), 2)
+
+print("Guessing key {}".format(bytes))
+
+for (pt, ct, _) in tuples:
+    pt = to_bytes(pt)
+    ct = to_bytes(ct)
+    if len(pt) != len(ct):
+        print("Length of messages does not match")
+
+    res = aes.decrypt(ct, bytes, 16)
+    if pt != res:
+        print("Got {} instead of {}".format(res, pt))
+        break
